9f6efba556d1cddb22d0aeb60b66ef34d4341027
avenirs05 <avenirs05@gmail.com>
2017-08-02 21:15:12 +0300

    last

--- wp-content/themes/lalin/page-fractional_rejuve.php
+++ wp-content/themes/lalin/page-fractional_rejuve.php
@@ -3,7 +3,7 @@
 <div class="container-fluid visible-md-block visible-lg-block">
     <div class="row">
         <div class="col-md-4 sidebar">
-            <?php require_once 'sidebar.php'; ?>
+            <?php require_once 'sidebar-equipment.php'; ?>
         </div>
         <div class="col-md-8 page-content">
             <?php $contentOfPage = get_page_by_title('Фракционное омоложение'); ?>
@@ -17,6 +17,9 @@
 </div> 
 
 <div class="container-fluid visible-xs-block visible-sm-block">
+    <h1 class="head-article text-center">
+        <?php echo $contentOfPage->post_title; ?>
+    </h1>
     <?php echo $contentOfPage->post_content; ?>
     <p id="price"><a href="price" target="_blank">Стоимость услуг</a></p>
 </div>
